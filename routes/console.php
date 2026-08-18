894739f341b32adf6324e13de3571bd39dfa95fb
hyfizh <hondadaun@gmail.com>
2026-08-18 09:52:08 +0700

    Register absensi:auto-alpha command explicitly in routes/console.php for Laravel 11

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\AutoAlphaCommand;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('absensi:auto-alpha', function () {
+    $this->call(AutoAlphaCommand::class);
+})->purpose('Otomatis menandai status Alpa (A) bagi mahasiswa yang tidak absen setelah jam matkul berakhir');
