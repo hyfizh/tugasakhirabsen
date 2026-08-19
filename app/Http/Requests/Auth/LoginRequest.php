1f8fe57811d7405ce149fcabe65922157f424477
hyfizh <hondadaun@gmail.com>
2026-08-19 08:04:37 +0700

    Fix: ubah pesan kesalahan login saat username atau password salah menjadi 'Password atau Username salah'

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -61,7 +61,7 @@ class LoginRequest extends FormRequest
                 RateLimiter::hit($this->throttleKey());
 
                 throw ValidationException::withMessages([
-                    'username' => trans('auth.failed'),
+                    'username' => 'Password atau Username salah',
                 ]);
             }
         }
